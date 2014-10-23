7817167e3000f1db9888c79f4ad81ea113e92eab
David <dleavenworth.ep@mysummitps.org>
2014-10-23 18:37:57 +0000

    -c

--- scroll.js
+++ scroll.js
@@ -1,15 +1,30 @@
-$("a").click(function() {
+/*$("a").click(function() {
   return $("html, body").animate({
-    scrollTop: $($.attr(this, "href")).offset().top
+  scrollTop: $($.attr(this, "href")).offset().top
   }, 500), window.location.hash = "", !1
-}), $(".scrollup").hide(), $(function() {
+  }), $(".scrollup").hide(), $(function() {
   $(window).scroll(function() {
-    $(this).scrollTop() > 100 ? $(".scrollup").fadeIn() : $(".scrollup").fadeOut()
+  $(this).scrollTop() > 100 ? $(".scrollup").fadeIn() : $(".scrollup").fadeOut()
   })
-});*/
+  });*/
 
-$('a').click(function(){
+/*$('a').click(function(){
   $('html', 'body').animate({
-    scrollTop: $($.attr(this, "href")).offset().top
+  scrollTop: $($.attr(this, "href")).offset().top
   }, 500);
+  });*/
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+    var target = $(this.hash);
+  target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+  if (target.length) {
+    $('html,body').animate({
+      scrollTop: target.offset().top
+    }, 1000);
+    return false;
+  }
+  }
+  });
 });

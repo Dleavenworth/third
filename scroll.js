03b41635d3ec774455429501013cf4a914e748af
David <dleavenworth.ep@mysummitps.org>
2014-10-23 17:40:21 +0000

    Commit because its broken

--- scroll.js
+++ scroll.js
@@ -1,4 +1,4 @@
-"use strict"$("a").click(function() {
+$("a").click(function() {
   return $("html, body").animate({
     scrollTop: $($.attr(this, "href")).offset().top
   }, 500), window.location.hash = "", !1
@@ -6,7 +6,7 @@
   $(window).scroll(function() {
     $(this).scrollTop() > 100 ? $(".scrollup").fadeIn() : $(".scrollup").fadeOut()
   })
-});
+});*/
 
 $('a').click(function(){
   $('html', 'body').animate({

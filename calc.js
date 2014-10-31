880c72122a27731216196317edf4d18b3c099b3d
David <dleavenworth.ep@mysummitps.org>
2014-10-31 15:26:44 -0400

    Big changes

---
+++ calc.js
@@ -0,0 +1,56 @@
+function show_cal(){
+
+  function num{
+    a=Number(document.form1.num1.value);
+    b=Number(document.form1.num2.value);
+    c=a+b;
+    document.form1.result1.value=c;
+
+    a=Number(document.form1.num1.value);
+    b=Number(document.form1.num2.value);
+    c=a-b;
+    document.form1.result2.value=c;
+
+    a=Number(document.form1.num1.value);
+    b=Number(document.form1.num2.value);
+    c=a*b;
+    document.form1.result3.value=c;
+
+    a=Number(document.form1.num1.value);
+    b-Number(document.form1.num1.value);
+    c=a/b;
+    document.form1.result4.value=c;
+  }
+
+  function addition() {
+    a=Number(document.form1.num3.value);
+    b=Number(document.form1.num4.value);
+    c=a+b;
+    document.form1.result1.value=c;
+  }
+
+  function subtraction() {
+    a=Number(document.form1.num5.value);
+    b=Number(document.form1.num6.value);
+    c=a-b;
+    document.form1.result2.value=c;
+  }
+
+  function multiply() {
+    a=Number(document.form1.num7.value);
+    b=Number(document.form1.num8.value);
+    c=a*b;
+    document.form1.result3.value=c;
+  }
+
+  function division() {
+    a=Number(document.form1.num9.value);
+    b-Number(document.form1.num10.value);
+    c=a/b;
+    document.form1.result4.value=c;
+  }
+  num();
+}
+
+
+

ff8a6e02e3d1b3b3c6c0da2b2904383e6f712a4b
Alex <me@alexb.ninja>
2014-11-01 16:21:13 -0700

    complete js & css & layout html

--- js/calc.js
+++ js/calc.js
@@ -1,4 +1,4 @@
-
+/*
 function show_cal() {
   function num() {
     a=Number(document.form1.num1.value);
@@ -19,7 +19,7 @@ function show_cal() {
     a=Number(document.form1.num1.value);
     b=Number(document.form1.num2.value);
     c=a/b;
-    document.form1.num1.value);
+    document.form1.num1.value=c;
   }
 
   function addition() {
@@ -50,4 +50,48 @@ function show_cal() {
     document.form1.result4.value=c;
   }
   num();
-}
+} */
+
+
+var num = function(type, number) {
+  return Number($('.'+type+'.'+number).val());
+};
+
+var add = function(a, b) {
+  return a + b;
+};
+
+var sub = function(a, b) {
+  return a - b;
+};
+
+var multi = function(a, b) {
+  return  a * b;
+};
+
+var div = function(a, b) {
+  return a / b;
+};
+
+var fadd = function() {
+  return add(num('add', 'first'), num('add', 'second'));
+};
+
+var fsub = function() {
+  return sub(num('sub', 'first'), num('sub', 'second'));
+};
+
+var fmulti = function() {
+  return multi(num('multi', 'first'), num('multi', 'second'));
+};
+
+var fdiv = function() {
+  return div(num('div', 'first'), num('div', 'second'));
+};
+
+var results = function() {
+  $('.add.result').text(fadd());
+  $('.sub.result').text(fsub());
+  $('.multi.result').text(fmulti());
+  $('.div.result').text(fdiv());
+};

5ec35f9b9e6e24edf4647fd2172467cc2a87b9e9
David <dleavenworth.ep@mysummitps.org>
2015-01-09 18:10:18 -0500

    Changed a lot

--- js/better.js
+++ js/better.js
@@ -2,35 +2,63 @@ var keys = document.querySelectorAll('a.button');
 for (var i = 0; i < keys.length; i++) {
   keys[i].addEventListener('click', mathify);
 }
-
 var output = document.getElementById('display');
 var input = [];
 
 function mathify() {
-  //First number
-  if (input === undefined) {
-    input[0] = this.innerHTML;
+  console.log(input);
+  var ans = undefined;
+  /// User hits = Sign ///
+  if (this.innerHTML === '=' && input.length === 3) {
+    console.log("equalspushed");
+    switch (input[1]) {
+      case '+':
+        ans = input[0] + input[2];
+      case '-':
+        ans = input[0] - input[2];
+      case '*':
+        ans = input[0] * input[2];
+      case '/':
+        ans = input[0] / input[2];
+      default:
+        ans = NaN;
+    }
+    output.innerHTML = ans;
+
+  } else if (this.innerHTML === 'clear') {
+    console.log("clearpushed");
+    output.innerHTML = '';
+    input = [];
   }
+  // Op //WORK ON THIS//
+  else if(input.length === 2) {
+    console.log("Operator");
+    input.push(this.innerHTML);
+  }
+
+
   //Second number
-  if (input.length === 1 && this.classList.contains === 'opp') {
+  else if (input.length === 1 && this.classList.contains === 'opp') {
+    console.log("op pushed");
     input[1] = this.innerHTML;
   }
-  else if (this.innerHTML === '=' && input.length === 3) {
-    switch(input[1]) {
-      case '+':
-        return input[0] + input[2];
-        //break;
-      case '-':
-        return input[0] - input[2];
-        //break;
-      case '*': 
-        return input[0] * input[2];
-        //break;
-      case '/': 
-        return input[0] / input[2];
-        //break;
-      default:
-        return "Error";
+  //First number
+  else if (input.length < 2) {
+    console.log("first number added/inputed");
+    if (input.length === 0) {
+      input.push(parseInt(this.innerHTML));
     }
+    else{
+      input[0] = input[0] * 10 + parseInt(this.innerHTML)
+    }
+    output.innerHTML = input[0];
   }
+  // this shouldn't ever run//
+  else {
+    //   output.insertAdjacentHTML('beforeend', this.innerHTML);
+    console.log("last else");
+    output.innerHTML += this.innerHTML
+  }
+  console.log(input)
 }
+console.clear();

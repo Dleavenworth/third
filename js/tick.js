2144e430e67d36434bebf85c74b9f25aa40c416a
David <dleavenworth.ep@mysummitps.org>
2015-01-15 18:22:23 -0500

    Changed for loops

--- js/tick.js
+++ js/tick.js
@@ -3,7 +3,7 @@ for (var i = 0; i < keys.length; i++) {
   keys[i].addEventListener('click', runGame);
 }
 var turns = 1;
-function runGame() {
+function runGame(evt) {
   if(turns % 2 === 0) {
     turns++;
     this.innerHTML = 'X';
@@ -15,10 +15,10 @@ function runGame() {
   if (turns === 10) {
     alert("Cat's game");
   }
-  winCheck();
+  winCheck(evt.target);
 }
 
-function winCheck() {
+function winCheck(targt) {
   var sq1 = document.getElementById('box0');
   var sq2 = document.getElementById('box1');
   var sq3 = document.getElementById('box2');
@@ -31,17 +31,17 @@ function winCheck() {
 
   if (sq1.innerHTML === sq2.innerHTML && sq2.innerHTML === sq3.innerHTML) {
     for(var i = 0; i < 9; i++){
-      document.getElementById('box' + i).innerHTML = this.innerHTML;
+      document.getElementById('box' + i).innerHTML = targt.innerHTML;
     }  
   }
   else if (sq4.innerHTML === sq5.innerHTML && sq5.innerHTML === sq6.innerHTML) {
     for(var i = 0; i < 9; i++) {
-      document.getElementById('box' + i).innerHTML = this.innerHTML;
+      document.getElementById('box' + i).innerHTML = targt.innerHTML;
     }
   }
   else if (sq7.innerHTML === sq8.innerHTML && sq8.innerHTML === sq9.innerHTML) {
     for(var i = 0; i < 9; i++) {
-      document.getElementById('box' + i).innerHTML = this.innerHTML;
+      document.getElementById('box' + i).innerHTML = targt.innerHTML;
     }
   }
   else if (sq1.innerHTML === sq5.innerHTML && sq5.innerHTML === sq9.innerHTML) {

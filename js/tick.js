b987ef3f8c2682dea21bf995dbd7d923fce76d01
David <dleavenworth.ep@mysummitps.org>
2015-01-16 14:46:42 -0500

    Changed things

--- js/tick.js
+++ js/tick.js
@@ -1,8 +1,10 @@
+//Adds event listener to all divs
 var keys = document.querySelectorAll('div.box');
 for (var i = 0; i < keys.length; i++) {
   keys[i].addEventListener('click', runGame);
 }
 var turns = 1;
+//Runs the game and places X's and O's on divs
 function runGame(evt) {
   if(turns % 2 === 0) {
     turns++;
@@ -17,7 +19,7 @@ function runGame(evt) {
   }
   winCheck(evt.target);
 }
-
+//Function checks for win 
 function winCheck(targt) {
   var sq1 = document.getElementById('box0');
   var sq2 = document.getElementById('box1');
@@ -29,27 +31,27 @@ function winCheck(targt) {
   var sq8 = document.getElementById('box7');
   var sq9 = document.getElementById('box8');
 
-  if (sq1.innerHTML === sq2.innerHTML && sq2.innerHTML === sq3.innerHTML) {
+  if (sq1.innerHTML === sq2.innerHTML && sq2.innerHTML === sq3.innerHTML && sq1.innerHTML.length === 1 ) {
     for(var i = 0; i < 9; i++) {
       document.getElementById('box' + i).innerHTML = targt.innerHTML;
     }  
   }
-  else if (sq4.innerHTML === sq5.innerHTML && sq5.innerHTML === sq6.innerHTML) {
+  else if (sq4.innerHTML === sq5.innerHTML && sq5.innerHTML === sq6.innerHTML && sq4.innerHTML.length === 1 ) {
     for(var i = 0; i < 9; i++) {
       document.getElementById('box' + i).innerHTML = targt.innerHTML;
     }
   }
-  else if (sq7.innerHTML === sq8.innerHTML && sq8.innerHTML === sq9.innerHTML) {
+  else if (sq7.innerHTML === sq8.innerHTML && sq8.innerHTML === sq9.innerHTML && sq7.innerHTML.length === 1) {
     for(var i = 0; i < 9; i++) {
       document.getElementById('box' + i).innerHTML = targt.innerHTML;
     }
   }
-  else if (sq1.innerHTML === sq5.innerHTML && sq5.innerHTML === sq9.innerHTML) {
+  else if (sq1.innerHTML === sq5.innerHTML && sq5.innerHTML === sq9.innerHTML && sq1.innerHTML.length === 1) {
     for( var i = 0; i < 9; i++) {
       document.getElementById('box' + i).innerHTML = this.innerHTML;
     }
   }
-  else if (sq3.innerHTML === sq5.innerHTML && sq5.innerHTML === sq7.innerHTML) {
+  else if (sq3.innerHTML === sq5.innerHTML && sq5.innerHTML === sq7.innerHTML && sq3.innerHTML.length === 1) {
     for(var i = 0; i < 9; i++) {
       document.getElementById('box' + i).innerHTML = this.innerHTML;
     }
